1d738e167e5a166502094bde63e0cdade9d4a333
Digerati <cabal@digerati.design>
2024-03-20 18:18:44 +0000

    Add skipToMainContent and formValidation classes

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,8 @@
+import { formValidation } from "$digerati/formValidation";
+import { skipToMainContent } from "$digerati/skipToMainContent";
+
 window.Webflow || [];
 window.Webflow.push(() => {
-  /* Prevent FOUT */
-  document.body.classList.add('webflow-loaded');
-  /* Skip to content link */
-  const trigger = document.querySelector('[dd-skip-to-main-content="trigger"]'),
-    target = document.querySelector('[dd-skip-to-main-content="target"]');
-  if (trigger && target) {
-    ['click', 'keypress'].forEach((event) => {
-      trigger.addEventListener(event, (e) => {
-        if (e.type === 'keydown' && e.which !== 13) {
-          return;
-        }
-        e.preventDefault();
-        target.setAttribute('tabindex', '-1');
-        target.focus();
-      });
-    });
-  }
+  skipToMainContent();
+  formValidation();
 });
